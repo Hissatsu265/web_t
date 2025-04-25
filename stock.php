5b15b5cb3c45947f43a3750b4a1e1ddf7ae254b8
Lê Minh Toàn <109668965+Hissatsu265@users.noreply.github.com>
2025-04-25 08:40:11 +0700

    Update stock.php

--- stock.php
+++ stock.php
@@ -1,6 +1,10 @@
 <?php
 class Stock {
- 
+    public float $open;
+    public float $high;
+    public float $low;
+    public float $close;
+    public string $ticker;
     
 // gdfgf
     public function __construct(string $ticker, float $open, float $high, float $low, float $close) {
